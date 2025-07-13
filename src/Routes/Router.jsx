df943bcb67b35c219fbc35db54d223eb5a3f22e0
Rakib-76 <mrrakibhasan76@gmail.com>
2025-07-14 01:22:17 +0600

    added the full front end code for admin role

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -24,12 +24,12 @@ import CreateNote from "../Pages/Dashboard/Student/CreateNote";
 import ViewBookedSessions from "../Pages/Dashboard/Student/ViewBookedSessions";
 import ManageNotes from "../Pages/Dashboard/Student/ManageNotes";
 import ViewAllMaterials from "../Pages/Dashboard/Student/ViewAllMaterials";
-import AdminViewAllMaterials from "../Pages/Dashboard/Admin/AdminViewAllMaterial";
 import StudySessions from "../Pages/Home/StudySession/StudySessions ";
 import SessionDetails from "../Pages/Home/Details/SessionDetails ";
 import TutorList from "../Pages/Home/TutorList/TutorList ";
 import AdminViewUsers from "../Pages/Dashboard/Admin/AdminViewUsers ";
 import AdminViewSessions from "../Pages/Dashboard/Admin/AdminViewSessions ";
+import AdminViewMaterials from "../Pages/Dashboard/Admin/AdminViewMaterials";
 
 
 export const router = createBrowserRouter([
@@ -96,7 +96,7 @@ export const router = createBrowserRouter([
       },
       {
         path: "materials",
-        element: <AdminViewAllMaterials></AdminViewAllMaterials>
+        element:<AdminViewMaterials></AdminViewMaterials>
       },
     ]
   },
